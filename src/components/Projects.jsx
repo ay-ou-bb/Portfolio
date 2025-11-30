3c81ec50f5170319f5c338f501ae4eca42723efe
ay-ou-bb <ayoubbo3333@gmail.com>
2025-11-30 18:24:02 +0100

    feat: add skills section and projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,38 @@
+import landingPage from "../assets/landingPage.png"
+import { LuExternalLink } from "react-icons/lu";
+
 export default function Projects(){
     return(
-        <div>
-            <h1>
-                Projects
-            </h1>
+        <div className="flex  flex-col mt-24">
+            <h1 className="mb-16">Projects</h1>
+            <div className="flex justify-around">
+                <div className="w-[25%] shadow-[5px_10px_15px_rgba(0,0,0,0.25)] p-4 bg-[#F8FAFC] border border-gray-100  rounded-xl">
+                    <img className="border border-gray-200" src={landingPage}/>
+                    <div className="p-2 m-2 flex flex-col justify-center items-center">
+                        <h2 className="text-2xl font-bold">TodoList</h2>
+                        <p className="m-2">this is my todo list to help other to orgaization dealy work</p> 
+                        <button className="bg-[#3B82F6] text-white flex justify-between items-center gap-2">Open Link <span><LuExternalLink/></span></button>
+                    </div>
+                </div>
+
+                <div className="w-[25%] shadow-[5px_10px_15px_rgba(0,0,0,0.25)] p-4 bg-[#F8FAFC] border border-gray-100  rounded-xl">
+                    <img className="border border-gray-200" src={landingPage}/>
+                    <div className="p-2 m-2 flex flex-col justify-center items-center">
+                        <h2 className="text-2xl font-bold">TodoList</h2>
+                        <p className="m-2">this is my todo list to help other to orgaization dealy work</p> 
+                        <button className="bg-[#3B82F6] text-white flex justify-between items-center gap-2">Open Link <span><LuExternalLink/></span></button>
+                    </div>
+                </div>
+
+                <div className="w-[25%] shadow-[5px_10px_15px_rgba(0,0,0,0.25)] p-4 bg-[#F8FAFC] border border-gray-100  rounded-xl">
+                    <img className="border border-gray-200" src={landingPage}/>
+                    <div className="p-2 m-2 flex flex-col justify-center items-center">
+                        <h2 className="text-2xl font-bold">TodoList</h2>
+                        <p className="m-2">this is my todo list to help other to orgaization dealy work</p> 
+                        <button className="bg-[#3B82F6] text-white flex justify-between items-center gap-2">Open Link <span><LuExternalLink/></span></button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,57 @@
+import React from "react"
+import "../style.css"
+
 export default function Skills(){
+    
     return(
-        <div>
-            <h1>
-                Skills
-            </h1>
+
+        <div className="w-full flex flex-col justify-center items-center mt-16">
+            <h1 className=" drop-shadow-[2px_5px_2px_rgba(59,130,246,1)]">My Skills</h1>
+            <div className="flex flex-col-reverse md:flex-row w-full justify-center items-center">
+                <div className="container relative flex justify-center items-center flex-col raduis-md w-[80%] md:w-[40%] py-[10px] px-[20px]">
+                    
+                    <div className="skill-box">
+                        <span className="title">HTML</span>
+                        <div className="skill-bar ">
+                            <span className="skill-per html">
+                                <span className="tooltip">95%</span>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="skill-box">
+                        <span className="title">CSS</span>
+                        <div className="skill-bar ">
+                            <span className="skill-per css">
+                                <span className="tooltip">80%</span>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="skill-box">
+                        <span className="title">JavaScript</span>
+                        <div className="skill-bar ">
+                            <span className="skill-per javascript">
+                                <span className="tooltip">60%</span>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="skill-box">
+                        <span className="title">React js</span>
+                        <div className="skill-bar ">
+                            <span className="skill-per react">
+                                <span className="tooltip">50%</span>
+                            </span>
+                        </div>
+                    </div>
+                
+                </div>
+                <div className="w-[80%] md:w-[40%] p-16 mt-16 md:mt-0 md:ml-16 bg-[#3B82F6] text-[#F8FAFC] rounded-xl shadow-[5px_10px_15px_rgba(0,0,0,0.25)]">
+                    <h1 className="mb-8">Overview</h1>
+                    <p>A snapshot of my core technical skills and the proficiency l've developed through consistent practice and real project experience.</p>
+                </div>
+            </div>
         </div>
     )
 }
