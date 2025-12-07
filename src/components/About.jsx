a162da350b5a8ac1c9d188b642c10e4b553a9e08
ay-ou-bb <ayoubbo3333@gmail.com>
2025-12-07 17:58:24 +0100

    style: add some style to de about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,9 +32,9 @@ useEffect(()=>{
                     
                     <img src={visible?imageAbout:aboutImageSmal} className="h-[70%] mb-8 md:mb-auto md:h-80 lg:h-auto drop-shadow-[0_5px_20px_rgba(59,130,246,1)]"/>
                 </div>
-                <div className="max-sm:w-full w-[33rem] text-left flex justify-center items-center flex-col md:block">
-                    <h2 className="w-[200px] md:w-96 absolute md:static text-2xl lg:text-[38px] max-md:text-center font-semibold mb-8 drop-shadow-[2px_5px_2px_rgba(59,130,246,1)] top-[38%]">I am Professional User Experience Designer</h2>
-                    <div className=" w-full text-sm md:text-base lg:text-lg font-normal max-md:text-center text-gray-600"><p>I design and develop services for customers specializing creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences.</p><p className="mt-3">I design and develop services for customers specializing creating stylish, modern websites, web services.</p></div>
+                <div className=" w-[90%] text-left flex justify-center items-center flex-col md:block">
+                    <h2 className="w-[45%] md:w-96 absolute md:static text-xl sm:text-2xl lg:text-[38px] max-md:text-center font-semibold mb-8 drop-shadow-[2px_5px_2px_rgba(59,130,246,1)] top-[25%] right-[18%]">I am Professional User Experience Designer</h2>
+                    <div className=" w-full text-sm md:text-base lg:text-lg font-normal flex justify-center items-center flex-col text-gray-600"><p>I design and develop services for customers specializing creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences.</p><p className="mt-3">I design and develop services for customers specializing creating stylish, modern websites, web services.</p></div>
                 </div>
             </div>
         </div>
