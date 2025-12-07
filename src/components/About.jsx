93d3450db24cf50a68adf4ed013e5ce4da83e236
ay-ou-bb <ayoubbo3333@gmail.com>
2025-12-07 18:14:31 +0100

    style: add some style to de about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ useEffect(()=>{
             <div className=" flex max-md:flex-col justify-around items-center gap-6 my-24 w-5/6 p-6 md:p-16 shadow-2xl">
                 <div >
                     
-                    <img src={visible?imageAbout:aboutImageSmal} className="h-[70%] mb-8 md:mb-auto md:h-80 lg:h-auto drop-shadow-[0_5px_20px_rgba(59,130,246,1)]"/>
+                    <img src={visible?imageAbout:aboutImageSmal} className="h-[60%] sm:h-[70%] mb-8 md:mb-auto md:h-80 lg:h-auto drop-shadow-[0_5px_20px_rgba(59,130,246,1)]"/>
                 </div>
                 <div className=" w-[90%] text-left flex justify-center items-center flex-col md:block">
                     <h2 className="w-[45%] md:w-96 absolute md:static text-xl sm:text-2xl lg:text-[38px] max-md:text-center font-semibold mb-8 drop-shadow-[2px_5px_2px_rgba(59,130,246,1)] top-[25%] right-[18%]">I am Professional User Experience Designer</h2>
