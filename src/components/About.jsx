c94aee8c797ec3ecb5b60097f4b507909feefb8a
ay-ou-bb <ayoubbo3333@gmail.com>
2025-12-12 18:17:42 +0100

    feat: add titel to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ useEffect(()=>{
                     <img src={visible?imageAbout:aboutImageSmal} className="h-[40%] w-full md:w-auto sm:h-[60%] mb-8 md:mb-auto md:h-80 lg:h-auto drop-shadow-[0_5px_20px_rgba(59,130,246,1)]"/>
                 </div>
                 <div className="w-[98%] md:w-[90%] text-left flex justify-center items-center flex-col md:block">
-                    <h2 className="w-[45%] md:w-96 absolute md:static  sm:text-xl md:text-2xl lg:text-[38px] max-md:text-center font-semibold mb-8 drop-shadow-[2px_5px_2px_rgba(59,130,246,1)] top-[22%] right-[18%]  sm:top-[23%] sm:right-[18%]">I am Professional User Experience Designer</h2>
+                    <h2 className="w-[45%] md:w-96 absolute md:static  sm:text-xl md:text-2xl lg:text-[38px] max-md:text-center font-semibold mb-8 drop-shadow-[2px_5px_2px_rgba(59,130,246,1)] top-[22%] right-[18%]  sm:top-[23%] sm:right-[18%]">I am front-End Devceloper </h2>
                     <div className=" w-full text-xs sm:text-sm md:text-base lg:text-lg font-normal flex justify-center items-center flex-col text-gray-600"><p>i'm a front-End Devceloper passionte about building clean, responsive, and user-friendly web interfaces. i enjoy turning ideas into real, functional designs using modern tools and best practices. I work with HTML, CSS, JavaScript, tailwind CSS, and React to Create fast and intuitive experiences that look great on all devices. I'm always learning and improving to deliver better, more polished work </p><p className="mt-3">I design and develop services for customers specializing creating stylish, modern websites, web services.</p></div>
                 </div>
             </div>
