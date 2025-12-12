975eab95082f3762c67c5322eb33597bbca451d5
ay-ou-bb <ayoubbo3333@gmail.com>
2025-12-12 18:13:56 +0100

    feat: add footer and style it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,56 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import AlternateEmailRoundedIcon from '@mui/icons-material/AlternateEmailRounded';
+import { FaGithub } from "react-icons/fa";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+
 export default function Footer (){
     return(
-        <div className="bg-[#0F172A] h-[150px] flex justify-center items-center">
-            <ul className="flex justify-center items-center gap-8 text-white">
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
+        <div className="bg-[#0F172A] h-[150px] flex flex-col justify-center items-center">
+           <div className=" w-[40%] flex justify-around items-center">
+                        <a
+                        href="https://www.linkedin.com/in/ayoub-el-hyaoui-24953b2b3/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="rounded-full"
+                        >
+                            <LinkedInIcon  style={{color:"#d1d5db",fontSize:"30px",cursor:"pointer",textColor:"black", borderRadius:"50%"}}/>
+                        </a>
+
+                         {/* <a
+                            href="https://www.linkedin.com/in/ayoub-el-hyaoui-24953b2b3/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <InstagramIcon style={{color:"#ee2a7b ",fontSize:"60px",cursor:"pointer"}}/>
+                        </a> */}
+
+                         <a
+                            href="https://web.facebook.com/ayo.ub.230749"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <FacebookIcon style={{color:"#d1d5db",fontSize:"30px",cursor:"pointer",textColor:"black", borderRadius:"50%"}}/>
+                        </a>
+
+                         <a
+                            href="https://github.com/ay-ou-bb"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <FaGithub style={{color:"#d1d5db",fontSize:"30px",cursor:"pointer",textColor:"black", borderRadius:"50%"}} />
+                        </a>
+
+                        <a
+                            href="mailto:ayoubbo3333@gmail.com"
+                            >
+                            <AlternateEmailRoundedIcon style={{color:"#d1d5db",fontSize:"30px",cursor:"pointer",textColor:"black", borderRadius:"50%"}} />
+                        </a>
+                        
+                        
+                    </div>
+                <p className="text-gray-300 mt-8">copyright&copy;2025</p>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,13 +42,13 @@ export default function Navbar(){
 
     return(
         
-        <div className="flex w-full justify-between md:justify-around  fixed top-0  pb-4  z-[999] bg-[#0F172A] mr-4  ">
+        <div className="flex w-full justify-between md:justify-around  fixed top-0  pb-2  z-[999] bg-[#0F172A] mr-4  ">
             <div className=" basis-1/5 flex justify-start">
             <h3 className="mt-6 ml-2  text-[#6366F1] font-bold ">{name}</h3>
             </div>
-            <div className="w-[70%]  p-2 mt-4 rounded-md "style={{display :visible ?"none":"block"}} >
+            <div className="w-[70%]  p-2 mt-2 rounded-md "style={{display :visible ?"none":"block"}} >
                 <ul className="flex justify-around w-[80%] bg-[#1E293B] " >
-                    <li className="cursor-pointer my-2  "><Link className=" text-white hover:text-[#6366F1] font-semibold hover:font-black"offset={-80} to="home" smooth={true} duration={500}>Home</Link></li>
+                    <li className="cursor-pointer my-2 hover:font-black"><Link className=" text-white hover:text-[#6366F1] font-semibold hover:font-black"offset={-80} to="home" smooth={true} duration={500}>Home</Link></li>
                     <li className="cursor-pointer my-2 "><Link style={{color:"white", fontWeight:"600"}} to="about" smooth={true} duration={500} >About</Link></li>
                     <li className="cursor-pointer my-2"><Link style={{color:"white", fontWeight:"600"}} to="skills" smooth={true} duration={500} >Skills</Link></li>
                     <li className="cursor-pointer my-2"><Link style={{color:"white", fontWeight:"600"}} to="project" smooth={true} duration={500} >Projects</Link></li>
@@ -56,7 +56,7 @@ export default function Navbar(){
                 </ul>
             </div>
             <div className=" mr-[-6px] basis-1/6 flex justify-end items-center  p-0 md:p-6 " style={{display:!visible?"none":"block"}}>
-                    <FaAlignJustify className="fixed text-white"  style={{display:!open?"block":"none", cursor:"pointer"}}  size={30}  
+                    <FaAlignJustify className="fixed text-white top-2 sm:top-0"  style={{display:!open?"block":"none", cursor:"pointer"}}  size={30}  
                     onClick={handleMenuSideOpen}/>
 
                     <FaXmark className="fixed text-white" size={30} style={{display:open?"block":"none", cursor:"pointer"}} onClick={handleMenuSideclose}/>
@@ -67,8 +67,3 @@ export default function Navbar(){
     )
 }
 
-//   <Hero/>
-//         <About/>
-//         <Skills/>
-//         <Projects/>
-//         <Contact/>
