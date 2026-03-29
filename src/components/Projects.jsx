a0ed55d16731287a16cfcfbb6e9c51fd00267ed9
ay-ou-bb <ayoubbo3333@gmail.com>
2026-03-29 22:04:37 +0000

    fix the width and revealOScroll in project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -40,31 +40,33 @@ export default function Projects(){
      ]
 
      return(
-        <RevealOnScroll>
-        <div id="projects" className="flex   flex-col mb-16">
+        
+        <div id="projects" className="flex flex-col mb-16">
             <h1 style={{color:theme.palette.secondary.dark,}} className="font-bold text-center text-2xl md:text-3xl mb-6 md:mb-12 lg:text-4xl drop-shadow-[2px_5px_2px_rgba(59,130,246,1)]">{t("projects.title")}</h1>
             
             {/* <div className="flex flex-col md:flex-row justify-center items-center md:justify-around"> */}
 
-            <div className="flex justify-center">
-                <div className="sm:w-[70%] md:w-[90%] grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="flex justify-center items-center w-full">
+                <div className="sm:w-[80%] md:w-[90%] grid grid-cols-1 md:grid-cols-3 gap-4 ">
             {/* PROJECTS */}
             {projectsArray.map((project)=>{
                 return(
-                    <div key={project.id} className="sm:w-[80%] md:w-auto h-96 flex justify-center flex-col items-center mb-12 md:mb-0 shadow-[5px_10px_15px_rgba(0,0,0,0.25)] p-2 bg-[#F8FAFC] border border-gray-100  rounded-xl">
-                        <img style={{border:`${theme.palette.primary.light} solid 3px`}} className="h-[50%] w-[90%] border border-gray-200" src={project.img}/>
-                        <div className="h-[45%] m-2 grid grid-rows-2 gap-4">
-                           <div className=" mt-4 h-"> 
-                                <h2 style={{color:theme.palette.primary.light}} className="text-xl md:text-2xl font-bold text-center">{t(`projects.${project.nameKey}`)}</h2>
-                                <p className="text-black m-2 text-sm md:text-base text-center">{t(`projects.${project.descKey}`)}</p> 
+                    <RevealOnScroll>
+                        <div key={project.id} className="sm:w-[80%] md:w-auto h-96 flex justify-center flex-col items-center mb-12 md:mb-0 shadow-[5px_10px_15px_rgba(0,0,0,0.25)] p-2 bg-[#F8FAFC] border border-gray-100  rounded-xl">
+                            <img style={{border:`${theme.palette.primary.light} solid 3px`}} className="h-[50%] w-[90%] border border-gray-200" src={project.img}/>
+                            <div className="h-[45%] m-2 grid grid-rows-2 gap-4">
+                            <div className=" mt-4 h-"> 
+                                    <h2 style={{color:theme.palette.primary.light}} className="text-xl md:text-2xl font-bold text-center">{t(`projects.${project.nameKey}`)}</h2>
+                                    <p className="text-black m-2 text-sm md:text-base text-center">{t(`projects.${project.descKey}`)}</p> 
+                                </div>
+                                <div className="flex justify-center items-center">
+                                    <Button className="flex justify-around items-center mb-4" href={project.link} variant="contained" endIcon={<OpenInNewIcon className="mr-2" />}>
+                                        {t("projects.open")}
+                                    </Button>
+                                </div>                            
                             </div>
-                             <div className="flex justify-center items-center">
-                                <Button className="flex justify-around items-center mb-4" href={project.link} variant="contained" endIcon={<OpenInNewIcon className="mr-2" />}>
-                                    {t("projects.open")}
-                                </Button>
-                            </div>                            
                         </div>
-                    </div>
+                     </RevealOnScroll>
                 )
             })}
 
@@ -72,7 +74,7 @@ export default function Projects(){
                 </div>
             </div>
         </div>
-        </RevealOnScroll>
+       
     )
 }
 
